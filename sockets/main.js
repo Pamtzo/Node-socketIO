e8208f6afecb7ed96e5b15c78ad815d695d317d1
Andres Felipe Ospina GUalteros <andresfelipe.ospina@utp.edu.co>
2021-04-14 20:41:29 -0500

    https

--- sockets/main.js
+++ sockets/main.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const http = require("http");
-const fs = require('fs');
-const bodyParser  = require("body-parser");
 const socketIo = require("socket.io");
-
-const options = {
-    key: fs.readFileSync('utils/key.pem'),
-    cert: fs.readFileSync('utils/cert.pem')
-};
 
 const index = require("../routes/index");
 const app = express();
 
+const https = require('https');
+const fs = require('fs');
+
 app.use(index);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const options = {
+  key: fs.readFileSync('utils/cert.key'),
+  cert: fs.readFileSync('utils/cert.crt')
+}
 
-const server = http.createServer(app);
+const server = https.createServer(options, app)
+
 const io = socketIo(server, { cors: {
     origin: '*',
   }});
