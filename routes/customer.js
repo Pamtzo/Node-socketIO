06ae46f8a9da7e17fceffe10fadbb4175c54e45e
Your Name <you@example.com>
2021-05-07 22:08:15 +0000

    refactor

--- routes/customer.js
+++ routes/customer.js
@@ -40,7 +40,7 @@ module.exports = function(app, io, authcode) {
         if(req.headers.authcode==authcode){
             console.log("reloadPage")
             console.log(req.body)
-            customerNamespace.to(req.body.id_company).emit("Company_refactor")
+            customerNamespace.to(req.body.id_company).emit("Company_refactor",{'is_maintenance':req.body.is_maintenance})
             res.send({ response: "Ok" }).status(200);
         }
         else{res.send({ response: "Bad authcode" }).status(403);}
